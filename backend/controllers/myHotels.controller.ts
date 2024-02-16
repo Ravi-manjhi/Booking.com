747ae9138329ff9c4ecc7a0bcbe7435625026e77
Ravi Manjhi <ravi.manjhi199@gmail.com>
2024-02-16 14:58:07 +0530

    added my hotel page

--- backend/controllers/myHotels.controller.ts
+++ backend/controllers/myHotels.controller.ts
@@ -2,6 +2,18 @@ import { v2 as cloudinary } from "cloudinary";
 import { Request, Response } from "express";
 import HotelModel, { HotelType } from "../model/hotel.model";
 
+async function uploadImages(imageFiles: Express.Multer.File[]) {
+  const uploadPromises = imageFiles.map(async (image) => {
+    const b64 = Buffer.from(image.buffer).toString("base64");
+    let dataURI = "data:" + image.mimetype + ";base64," + b64;
+    const res = await cloudinary.uploader.upload(dataURI);
+    return res.url;
+  });
+
+  const imageUrls = await Promise.all(uploadPromises);
+  return imageUrls;
+}
+
 export const createHotels = async (req: Request, res: Response) => {
   try {
     const imageFiles = req.files as Express.Multer.File[];
@@ -17,20 +29,22 @@ export const createHotels = async (req: Request, res: Response) => {
     await hotel.save();
 
     res.status(201).send(hotel);
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
 
-async function uploadImages(imageFiles: Express.Multer.File[]) {
-  const uploadPromises = imageFiles.map(async (image) => {
-    const b64 = Buffer.from(image.buffer).toString("base64");
-    let dataURI = "data:" + image.mimetype + ";base64," + b64;
-    const res = await cloudinary.uploader.upload(dataURI);
-    return res.url;
-  });
+export const getMyHotelCtrl = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
 
-  const imageUrls = await Promise.all(uploadPromises);
-  return imageUrls;
-}
+    const hotels = await HotelModel.find({ userId }).sort({ lastUpdated: -1 });
+    if (!hotels) {
+      return res.status(400).json({ message: "Bad Request" });
+    }
+
+    res.status(200).json(hotels);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
--- backend/model/hotel.model.ts
+++ backend/model/hotel.model.ts
@@ -17,21 +17,24 @@ export interface HotelType {
   lastUpdated: Date;
 }
 
-const HotelSchema = new Schema<HotelType>({
-  userId: { type: mongoose.Types.ObjectId, required: true },
-  name: { type: String, required: true },
-  city: { type: String, required: true },
-  country: { type: String, required: true },
-  description: { type: String, required: true },
-  type: { type: String, required: true },
-  adultCount: { type: Number, required: true },
-  childCount: { type: Number, required: true },
-  facilities: [{ type: String, required: true }],
-  pricePerNight: { type: Number, required: true },
-  starRating: { type: Number, required: true, min: 1, max: 5 },
-  imageUrls: [{ type: String, required: true }],
-  lastUpdated: { type: Date, required: true },
-});
+const HotelSchema = new Schema<HotelType>(
+  {
+    userId: { type: mongoose.Types.ObjectId, required: true },
+    name: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    description: { type: String, required: true },
+    type: { type: String, required: true },
+    adultCount: { type: Number, required: true },
+    childCount: { type: Number, required: true },
+    facilities: [{ type: String, required: true }],
+    pricePerNight: { type: Number, required: true },
+    starRating: { type: Number, required: true, min: 1, max: 5 },
+    imageUrls: [{ type: String, required: true }],
+    lastUpdated: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
 
 const HotelModel = model<HotelType>("Hotels", HotelSchema);
 
